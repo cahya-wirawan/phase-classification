9a171c3f70214ed94c73273f0f39485f1f4a0ce4
Cahya Wirawan <cahya.wirawan@gmail.com>
2018-02-26 16:56:26 +0100

    Added the code to split wavelets file to train and test datasets

--- phase_wavelet_spliter.py
+++ phase_wavelet_spliter.py
@@ -1,33 +1,41 @@
+import h5py
 from random import shuffle, seed
 
 
 def phase_spliter(filename, filename_training, filename_test, seed_number=10):
-    ds_test_counter = {'LPAZ-regP': 160, 'LPAZ-regS': 160, 'LPAZ-tele': 160, 'LPAZ-N': 480,
-                'URZ-regP': 2280, 'URZ-regS': 2280, 'URZ-tele': 2280, 'URZ-N': 6840}
+    ds_test_counter = {'LPAZ-regP': 10, 'LPAZ-regS': 10, 'LPAZ-tele': 10, 'LPAZ-N': 20,
+                'URZ-regP': 10, 'URZ-regS': 10, 'URZ-tele': 10, 'URZ-N': 20}
 
     seed(seed_number)
-    with open(filename, "r") as file:
-        f_train = open(filename_training, "w")
-        f_test = open(filename_test, "w")
-        line = file.readline()
-        f_train.write(line)
-        f_test.write(line)
-        lines = file.readlines()
-        shuffle(lines)
-        for line in lines:
-            row = line.split(',')
-            index = "{}-{}".format(row[1].strip('"'), row[6].strip('"'))
-            if index in ds_test_counter and row[8].strip('"') != "M":
-                if ds_test_counter[index] == 0:
-                    f_train.write(line)
-                else:
-                    f_test.write(line)
-                    ds_test_counter[index] -= 1
+    with h5py.File(filename, "r") as f:
+        f_train = h5py.File(filename_training, "w")
+        f_test = h5py.File(filename_test, "w")
+        station_group = f["/station"]
+        for station in station_group:
+            phase_group = station_group[station]
+            for phase in phase_group:
+                arids_group = phase_group[phase]
+                arids_length = len(arids_group)
+                arids = list(arids_group)
+                shuffle(arids)
+                index = "{}-{}".format(station, phase)
+                ds_test_counter[index] = min(arids_length, ds_test_counter[index])
+                for arid in arids:
+                    if index in ds_test_counter:
+                        wavelets = arids_group[arid]
+                        if ds_test_counter[index] == 0:
+                            f_train.create_dataset("/station/{}/{}/{}".format(station, phase, arid), data=wavelets)
+                        else:
+                            f_test.create_dataset("/station/{}/{}/{}".format(station, phase, arid), data=wavelets)
+                            ds_test_counter[index] -= 1
+
+        f_train.close()
+        f_test.close()
 
 
 if __name__ == "__main__":
-    filename = "data/phase/ml_features.csv"
-    filename_training = "data/phase/ml_features_train.csv"
-    filename_test = "data/phase/ml_features_test.csv"
+    filename = "data/phase/wavelets.hdf5"
+    filename_training = "data/phase/wavelets_train.hdf5"
+    filename_test = "data/phase/wavelets_test.hdf5"
 
     phase_spliter(filename, filename_training, filename_test)
