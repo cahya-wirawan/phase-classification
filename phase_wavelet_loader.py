e279c8f466c1b6126e8c2be5401e23c7db626989
Cahya Wirawan <cahya.wirawan@gmail.com>
2018-02-28 01:21:15 +0100

    added log

--- phase_wavelet_loader.py
+++ phase_wavelet_loader.py
@@ -4,6 +4,7 @@ from keras.utils import np_utils
 import h5py
 import random
 from scipy.stats import describe
+from math import log10
 
 
 class PhaseWaveletLoader(object):
@@ -59,7 +60,6 @@ class PhaseWaveletLoader(object):
                 bhz_stats = [None]*len(bhz)
                 bhn_stats = [None]*len(bhn)
                 for i in range(len(bhe)):
-                    """
                     stats = list(describe(bhe[i], axis=None))
                     bhe_stats[i] = stats[2:]
                     bhe_stats[i].extend(stats[1])
@@ -69,10 +69,10 @@ class PhaseWaveletLoader(object):
                     stats = list(describe(bhn[i], axis=None))
                     bhn_stats[i] = stats[2:]
                     bhn_stats[i].extend(stats[1])
-                    """
-                    bhe_stats[i] = [1]*6
-                    bhz_stats[i] = [1]*6
-                    bhn_stats[i] = [1]*6
+                    for j in range(len(bhe_stats[i])):
+                        bhe_stats[i][j] = log10(1+abs(bhe_stats[i][j]))
+                        bhz_stats[i][j] = log10(1+abs(bhz_stats[i][j]))
+                        bhn_stats[i][j] = log10(1+abs(bhn_stats[i][j]))
                     wavelet_max = max(abs(bhe[i].min()), bhe[i].max())
                     wavelet_max = max(wavelet_max, max(abs(bhz[i].min()), bhz[i].max()))
                     wavelet_max = max(wavelet_max, max(abs(bhn[i].min()), bhn[i].max()))
