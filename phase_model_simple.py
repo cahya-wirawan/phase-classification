55caec72c39ff5a2b784f89d868ba02b3437988a
Cahya Wirawan <cahya.wirawan@gmail.com>
2018-03-07 11:44:52 +0100

    updated the simple model

--- phase_model_simple.py
+++ phase_model_simple.py
@@ -4,7 +4,7 @@ from keras.layers import Dense
 
 
 # define baseline model
-def model_simple(layers, dropout=0.1):
+def model_simple(layers, dropout=0.1, layer_number=None):
     # create model
     model = Sequential()
     model.add(Dense(layers[0], input_shape=(1, 16), activation='relu'))
